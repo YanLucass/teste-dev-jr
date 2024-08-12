278799c558e9765ff7185aa10d33070479323d3c
YanLucass <yanlucascarvalho18@gmail.com>
2024-08-12 03:26:11 -0300

    fix: replace Next.js Image component with <img> tag to resolve  prop issue

--- frontend-cloudged/src/components/navbar.tsx
+++ frontend-cloudged/src/components/navbar.tsx
@@ -1,7 +1,6 @@
 // components/Navbar.tsx
 import React from "react";
 import Link from "next/link";
-import Image from "next/image"; // Para usar imagens otimizadas com Next.js
 import logo from "../../public/images/logo.png";
 
 const Navbar = () => {
@@ -15,7 +14,7 @@ const Navbar = () => {
       }}
     >
       <Link href="/">
-        <Image src={logo} alt="Cloudged" width={500} height={200} />
+        <img src={logo.src} alt="Cloudged" width={500} height={200} />
       </Link>
     </nav>
   );
